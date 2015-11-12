7c6995158c4ea62ef5c31ec527f1f81b1615fd97
ykjang <yongkyu.jang@gmail.com>
2015-11-12 17:12:51 +0900

    test

--- src/main/java/com/mailplants/admin/users/service/UserInfoServiceImpl.java
+++ src/main/java/com/mailplants/admin/users/service/UserInfoServiceImpl.java
@@ -4,7 +4,7 @@ package com.mailplants.admin.users.service;
 import java.util.HashMap;
 import java.util.List;
 
-import org.apache.log4j.Logger;
+//import org.apache.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -16,7 +16,7 @@ import com.mailplants.admin.users.domain.UserInfo;
 @Service("userInfoService")
 public class UserInfoServiceImpl implements UserInfoService {
 
-	private Logger logger = Logger.getLogger(getClass());
+	//private Logger logger = Logger.getLogger(getClass());
 	
 	@Autowired
 	protected UserInfoDao userInfoDao;
